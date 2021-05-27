df77c5070f06702f2a7f7aea82635b7008bd9421
nhenderson24 <nhenderson@bigassfans.com>
2021-05-27 14:04:27 -0400

    1st commit, Notes taken while larn'n

---
+++ hello_world/build/CMakeFiles/greetings.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/greetings.dir/hello.c.o"
+  "greetings"
+  "greetings.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/greetings.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ hello_world/hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void say_hello(void)
+{
+	printf("Oh, hello!\n");
+}
+
+int main()
+{
+	say_hello();
+	return(0);
+}
